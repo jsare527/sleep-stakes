18084bc15c4fa2f58ed05001bdca5af4c094f4dc
Jerad Bilek <jeradtripp@gmail.com>
2025-05-11 23:32:42 -0500

    Test: Add new migrations for vote and hangout models

--- app/authentication/models.py
+++ app/authentication/models.py
@@ -5,10 +5,35 @@ from django.dispatch import receiver
 from django.db.models.signals import post_save
 from rest_framework.authtoken.models import Token
 
+
 class User(AbstractUser):
     def __str__(self):
         return self.username
-    
+
+
+class Hangout(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='hangouts', on_delete=models.SET_NULL, null=True, blank=True) # noqa
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Vote(models.Model):
+    hangout = models.ForeignKey(Hangout, related_name='votes', on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='vote_made', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('hangout', 'user')
+
+    def __str__(self):
+        return f"{self.user.username} voted for {self.hangout.name}"
+
+
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_auth_token(sender, instance=None, created=False, **kwargs):
     if created:
